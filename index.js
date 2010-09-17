16099c1f149782e2a9bea50f9f153aa0b2f5eebc
Oliver Morgan <oliver.morgan@kohark.com>
2010-09-17 13:44:48 +0100

    Bumped to 0.3.0

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var crypto = require('crypto'),
 	cache  = { };
 
 // Defines parrot's version
-exports.version = '0.2.2';
+exports.version = '0.3.0';
 
 // Global configuration
 exports.config = {
